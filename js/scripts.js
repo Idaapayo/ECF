fe49758af5e5f236018a64675c1a389f1eecaa1b
peter soleeh <petersoleeh@gmail.com>
2017-08-30 17:50:15 +0300

    loans updating the loan section for finance department

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,46 @@ $(document).ready(function () {
 
 
   })
+  $(".no").click(function() {
+    $(".dates").hide();
 
+  });
+
+  $(".yes").click(function(){
+    $(".dates").show();
+  });
+
+
+var disbursed = ("input#disbursed");
+var repaid = ("input#repaid");
+function Amount(disbursed, repaid) {
+  this.disbursed = disbursed;
+  this.repaid = repaid;
+}
+
+function difference () {
+  var result = (disbursed - repaid)
+  return(result)
+}
+
+if($(disbursed === 0)
+ alert('As you have no loans with us, you are cleared pending final approval. An e-mail has been sent to the head of department for finalisation')
+
+  else if
+    $(result === 0) {
+      alert("As you have no loans with us, you are cleared pending final approval. An e-mail has been sent to the head of department for finalisation")
+  }
+    else if
+     $(repaid < disbursed) {
+       alert("You are currently not cleared due to the pending loan repayment. An e-mail has been sent to the head of department for follow up")
+     }
+   });
 });
+
+
+
+// if (true) {
+//
+// } else {
+//
+// }
